989846f6f2908e85df53e1b933e42f3d87edd06b
Maarten Narain <mnarain@qualogy.com>
2021-05-04 12:25:00 -0300

    contry list populaten

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
 let URL = 'https://restcountries.eu/rest/v2/';
 const countryListdiv = document.getElementById('countryList');
+const countryListDropdown = document.getElementById('countries-list');
 
 fetch(URL)
-  .then(res => res.json())
-  .then(json => {
-    countryList = json;
-    let counter = 1;
-    countryList.forEach(country => {
-      console.log(country.name + '    ' + country.flag);
-      addCountryDetails(country, counter);
-      populate(country);
-      counter++;
+    .then(res => res.json())
+    .then(json => {
+        const data = json;
+        countryListDropdown.length = 0;
+
+        let defaultOption = document.createElement('option');
+        defaultOption.text = 'Choose country';
+
+        countryListDropdown.add(defaultOption);
+        countryListDropdown.selectedIndex = 0;
+
+
+        let option;
+        for (let i = 0; i < data.length; i++) {
+            option = document.createElement('option');
+            option.text = data[i].name;
+            option.value = data[i];
+            countryListDropdown.add(option);
+        }
+
+        let counter = 1;
+        /*    countryList.forEach(country => {
+              console.log(country.name + '    ' + country.flag);
+              addCountryDetails(country, counter);
+              populate(country);
+              counter++;
+            });*/
+    })
+    .catch(function (error) {
+        console.log('error message: ' + error);
     });
-  })
-  .catch(function (error) {
-    console.log('error message: ' + error);
-  });
 
 function populate(country) {
-  const countryList = document.getElementById('countries-list');
-  document.getElementById('countries-list').innerHTML = countryList;
+    const countryList = document.getElementById('countries-list');
+    document.getElementById('countries-list').innerHTML = countryList;
 }
 
 /*        let countryList;
@@ -38,23 +56,28 @@ function populate(country) {
         countryList.forEach(country => console.log(country.name));*/
 
 function addCountryDetails(country, counter) {
-  //add country name
-  let countryHeader = document.createElement('h2');
-  let countryName = document.createTextNode(country.name);
-  countryHeader.appendChild(countryName);
-  countryHeader.id = 'cn' + counter;
-  countryHeader.className = 'displayInline';
-  countryListdiv.appendChild(countryHeader);
+    //add country name
+    let countryHeader = document.createElement('h2');
+    let countryName = document.createTextNode(country.name);
+    countryHeader.appendChild(countryName);
+    countryHeader.id = 'cn' + counter;
+    countryHeader.className = 'displayInline';
+    countryListdiv.appendChild(countryHeader);
 
-  //add countryflag
-  let countryFlag = document.createElement('img');
-  countryFlag.id = 'cf' + counter;
-  countryFlag.src = country.flag;
-  countryFlag.width = '50';
-  countryFlag.className = 'displayInline';
-  countryListdiv.appendChild(countryFlag);
+    //add countryflag
+    let countryFlag = document.createElement('img');
+    countryFlag.id = 'cf' + counter;
+    countryFlag.src = country.flag;
+    countryFlag.width = '50';
+    countryFlag.className = 'displayInline';
+    countryListdiv.appendChild(countryFlag);
 
-  let seperator = document.createElement('div');
-  seperator.className = 'seperator';
-  countryListdiv.appendChild(seperator);
+    let seperator = document.createElement('div');
+    seperator.className = 'seperator';
+    countryListdiv.appendChild(seperator);
+}
+
+function showCountryDetails() {
+    let country = countryListDropdown.value;
+    console.log(country);
 }
