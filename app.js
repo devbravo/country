0901e41f7fefdd1590df2beb4263bbb2692602b2
Diego <diegofranco711@gmail.com>
2021-05-05 14:46:47 -0300

    Modified code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const countryListdiv = document.getElementById('countryList');
+const countryList_div = document.getElementById('countryList');
 const countryListDropdown = document.getElementById('countries-list');
 const countryFlag = document.getElementById('country-flag');
+
 countryListDropdown.length = 0;
 
 const defaultOption = document.createElement('option');
@@ -10,36 +11,40 @@ countryListDropdown.add(defaultOption);
 
 const url = 'https://restcountries.eu/rest/v2/';
 
-fetch(url).then(response => {
-  if (response.status !== 200) {
-    console.warn(
-      'Looks like there was a problem. Status code: ',
-      response.status
-    );
-    return;
-  }
+fetch(url)
+  .then(response => {
+    if (response.status !== 200) {
+      console.warn(
+        'Looks like there was a problem. Status code: ',
+        response.status
+      );
+      return;
+    }
 
-  let option;
-  response.json().then(function (data) {
-    data.forEach(data => {
-      option = document.createElement('option');
-      option.text = data.name;
-      option.value = JSON.stringify(data);
-      countryListDropdown.add(option);
+    let option;
+    response.json().then(function (data) {
+      data.forEach(data => {
+        option = document.createElement('option');
+        option.text = data.name;
+        option.value = JSON.stringify(data);
+        countryListDropdown.add(option);
+      });
     });
+  })
+  .catch(function (error) {
+    console.error('Fetch Error - ', error);
   });
-});
 
-function showCountryDetails() {
-  let country = JSON.parse(countryListDropdown.value);
+const showCountryDetails = () => {
+  const country = JSON.parse(countryListDropdown.value);
   countryFlag.src = country.flag;
+};
 
-  //let countryFlag = document.createElement('img');
-  //countryFlag.width = '50';
-  //countryFlag.src = country.flag;
-  //document.getElementById('countryList__flag').appendChild(countryFlag);
-  //console.log(country.flag);
-}
+//let countryFlag = document.createElement('img');
+//countryFlag.width = '50';
+//countryFlag.src = country.flag;
+//document.getElementById('countryList__flag').appendChild(countryFlag);
+//console.log(country.flag);
 
 // function addCountryDetails(country) {
 //   //add countryflag
