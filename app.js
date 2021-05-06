20b5954ab6b51fcdd93a7ae8243bb021b928ef36
Diego <diegofranco711@gmail.com>
2021-05-06 15:55:17 -0300

    Added regional blocs, borders and installed openLayer with npm

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const countryList_div = document.getElementById('countryList');
 const countryListDropdown = document.getElementById('countries-list');
 const countryFlag = document.getElementById('country-flag');
+const countryCapital = document.getElementById('capital');
+const countryTimezone = document.getElementById('timezone');
+const countryLanguage = document.getElementById('language');
+const countryCurrency = document.getElementById('currency');
+const countryBorders = document.getElementById('borders');
+const countryBlocs = document.getElementById('regionalBlocs');
 
 countryListDropdown.length = 0;
 
@@ -35,26 +41,33 @@ fetch(url)
     console.error('Fetch Error - ', error);
   });
 
+let list;
+let blocs;
 const showCountryDetails = () => {
   const country = JSON.parse(countryListDropdown.value);
   countryFlag.src = country.flag;
+
+  // country capital info
+  countryCapital.innerText = `Capital: ${country.capital}`;
+  countryTimezone.innerText = `Timezone: ${country.timezones[0]}`;
+  countryLanguage.innerText = `Language: ${country.languages[0].name}`;
+  countryCurrency.innerText = `Currency: ${country.currencies[0].name}`;
+
+  // country borders
+  countryBorders.innerHTML = ' '; // clear previous elements after state change
+  country.borders.forEach(data => {
+    list = document.createElement('li');
+    list.innerText = data;
+    countryBorders.appendChild(list);
+  });
+
+  // Regional Blocs
+  regionalBlocs.innerHTML = ' '; // reset
+  country.regionalBlocs.forEach(data => {
+    let row = regionalBlocs.insertRow(0);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    cell1.innerText = data.acronym;
+    cell2.innerText = data.name;
+  });
 };
-
-//let countryFlag = document.createElement('img');
-//countryFlag.width = '50';
-//countryFlag.src = country.flag;
-//document.getElementById('countryList__flag').appendChild(countryFlag);
-//console.log(country.flag);
-
-// function addCountryDetails(country) {
-//   //add countryflag
-//   let countryFlag = document.createElement('img');
-//   countryFlag.src = country.flag;
-//   countryFlag.width = '50';
-//   countryFlag.className = 'displayInline';
-//   countryListdiv.appendChild(countryFlag);
-
-//   let seperator = document.createElement('div');
-//   seperator.className = 'seperator';
-//   countryListdiv.appendChild(seperator);
-// }
