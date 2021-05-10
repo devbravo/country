50d911a3e4ebe0388bc9f83b1ada60992346bde8
Maarten Narain <mnarain@qualogy.com>
2021-05-09 22:08:10 -0300

    minor refactoring
    TODO: Zet de informatie in tables en gebruik de hele bopdy van de webpage.
    TODO: bouw een kleine php rest api met onderstaande info.
    download en install:
    https://www.apachefriends.org/download.html pak de latest version
    https://www.slimframework.com/
    
    app 1. Bouw je eigen country api met data geformateerd zoals jij dat wil.
    app 2. Challenge bouw een JSON parser die de country api aanspreekt en alle ilanden ophaalt. Vervolgens moet deze de land velden die jij belangrijkvind opslaan in een php array en deze wegschrijven naar een file genaamd country_database.php.
    app1 1. Challenge lees de country_database php file uit wanneer een call word gedaan naar alle landen of 1 land middels een rest call.

--- app.js
+++ app.js
@@ -1,84 +1,81 @@
 const countryListDropdown = document.getElementById('countries-list');
+countryListDropdown.length = 0;
+const defaultOption = document.createElement('option');
+defaultOption.text = 'Choose Country';
+countryListDropdown.add(defaultOption);
 const countryFlag = document.getElementById('country-flag');
 const countryCapital = document.getElementById('capital');
 const countryTimezone = document.getElementById('timezone');
 const countryLanguage = document.getElementById('language');
 const countryCurrency = document.getElementById('currency');
 const countryBorders = document.getElementById('borders');
-const countryBlocs = document.getElementById('regionalBlocs');
-
-countryListDropdown.length = 0;
-
-const defaultOption = document.createElement('option');
-defaultOption.text = 'Choose Country';
-
-countryListDropdown.add(defaultOption);
-
 const url = 'https://restcountries.eu/rest/v2/';
+let map;
+let list;
+
+//Constants
+const responseNOT200 = 'Looks like there was a problem. Status code: ';
+
+function populateCountrydropdown(data) {
+    let option = document.createElement('option');
+    option.text = data.name;
+    option.value = JSON.stringify(data);
+    countryListDropdown.add(option);
+}
 
 fetch(url)
-  .then(response => {
-    if (response.status !== 200) {
-      console.warn(
-        'Looks like there was a problem. Status code: ',
-        response.status
-      );
-      return;
-    }
-
-    let option;
-    response.json().then(function (data) {
-      data.forEach(data => {
-        option = document.createElement('option');
-        option.text = data.name;
-        option.value = JSON.stringify(data);
-        countryListDropdown.add(option);
-      });
+    .then(response => {
+        if (response.status !== 200) {
+            console.warn(
+                responseNOT200,
+                response.status
+            );
+            return;
+        }
+        response.json().then(function (data) {
+            data.forEach(data => {
+                populateCountrydropdown(data);
+            });
+        });
+    })
+    .catch(function (error) {
+        console.error('Error: ', error);
     });
-  })
-  .catch(function (error) {
-    console.error('Fetch Error - ', error);
-  });
 
-let list;
-let blocs;
 const showCountryDetails = () => {
-  const country = JSON.parse(countryListDropdown.value);
-  countryFlag.src = country.flag;
+    const country = JSON.parse(countryListDropdown.value);
+    countryFlag.src = country.flag;
 
-  // country capital info
-  countryCapital.innerText = `Capital: ${country.capital}`;
-  countryTimezone.innerText = `Timezone: ${country.timezones[0]}`;
-  countryLanguage.innerText = `Language: ${country.languages[0].name}`;
-  countryCurrency.innerText = `Currency: ${country.currencies[0].name}`;
+    // country capital info
+    countryCapital.innerText = `Capital: ${country.capital}`;
+    countryTimezone.innerText = `Timezone: ${country.timezones[0]}`;
+    countryLanguage.innerText = `Language: ${country.languages[0].name}`;
+    countryCurrency.innerText = `Currency: ${country.currencies[0].name}`;
 
-  // country borders
-  countryBorders.innerHTML = ' '; // clear previous elements after state change
-  country.borders.forEach(data => {
-    list = document.createElement('li');
-    list.innerText = data;
-    countryBorders.appendChild(list);
-  });
+    // country borders
+    countryBorders.innerHTML = ''; // clear previous elements after state change
+    country.borders.forEach(data => {
+        list = document.createElement('li');
+        list.innerText = data;
+        countryBorders.appendChild(list);
+    });
 
-  // Regional Blocs
-  regionalBlocs.innerHTML = ' '; // reset
-  country.regionalBlocs.forEach(data => {
-    let row = regionalBlocs.insertRow(0);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    cell1.innerText = data.acronym;
-    cell2.innerText = data.name;
-  });
-
-  initMap(parseFloat(country.latlng[0]), parseFloat(country.latlng[1]));
+    // Regional Blocs
+    regionalBlocs.innerHTML = ' '; // reset
+    country.regionalBlocs.forEach(data => {
+        let row = regionalBlocs.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        cell1.innerText = data.acronym;
+        cell2.innerText = data.name;
+    });
+    initMap(parseFloat(country.latlng[0]), parseFloat(country.latlng[1]));
 };
 
-let map;
-
 function initMap(lat = 0, lng = 0) {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: lat, lng: lng },
-    zoom: 5,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-  });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: lat, lng: lng},
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+    });
 }
